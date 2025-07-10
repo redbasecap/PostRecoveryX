5c53ad441b7cf71da3f8857d210b010bb834068f
Nicola Spieser <spieser.nicola@gmail.com>
2025-07-10 13:26:13 +0200

    Initial Commit

---
+++ PostRecoveryXTests/PostRecoveryXTests.swift
@@ -0,0 +1,17 @@
+//
+//  PostRecoveryXTests.swift
+//  PostRecoveryXTests
+//
+//  Created by Nicola Spieser on 10.07.2025.
+//
+
+import Testing
+@testable import PostRecoveryX
+
+struct PostRecoveryXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
